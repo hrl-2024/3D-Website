2776f03cc89d38066e04cb607c30e3e7a1982a3c
Ruihang Liu <hrl@bu.edu>
2024-08-30 11:25:38 -0400

    Get the model rendered on screen

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+Additional package used to build the project:
+# for react skeletons
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+npm install react-router-dom  
+
+# for 3d model:
+npm install @react-three/fiber
+npm install @react-three/drei
+
+npm install @react-spring/three      # for animation
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
+import Island from '../models/island'
+
+{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        POPUP
+      </div> */}
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9]
+    } else {
+      screenScale = [1, 1, 1]
+    }
+
+    return [screenScale, screenPosition, rotation];
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+      <Canvas 
+        className='w-full h-screen bg-transparent'
+        camera={{ near: 0.1, far: 1000}}  // things that is closer than 0.1 or further than 1000 WON'T be rendered
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island 
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation} />
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
