7bc5e3699bcbbe585f5007323e7e1b5d103185e9
Ruihang Liu <hrl@bu.edu>
2024-09-02 16:49:10 -0400

    captalize component jsx name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
-import Island from '../models/island'
+import Island from '../models/Island'
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP
