b0c1fca3bbaccd17bb3e9b1fb21ea2e06ed24e39
Ruihang Liu <hrl@bu.edu>
2024-09-02 16:57:33 -0400

    sky component

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,9 +1,16 @@
+import { useGLTF } from '@react-three/drei'
 import React from 'react'
+
+import skyScene from '../assets/3d/sky.glb'
 
 const Sky = () => {
 
+  const sky = useGLTF(skyScene)
+
   return (
-    <mesh> </mesh>
+    <mesh>
+      <primitive object={sky.scene}/>
+    </mesh>
   )
 }
 
