2f44a5de747566ba1b95303c1a0a277d2fdabb69
Ruihang Liu <hrl@bu.edu>
2024-09-02 16:54:57 -0400

    Fix capital letters

---
+++ src/models/Sky.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Sky = () => {
+
+  return (
+    <mesh> </mesh>
+  )
+}
+
+export default Sky
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
+import Sky from '../models/Sky'
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP
@@ -34,6 +35,8 @@ const Home = () => {
           <directionalLight position={[1,1,1]} intensity={2}/>
           <ambientLight intensity={0.5}/>  {/* This makes everything brighter but not creating any shadow */}
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
+
+          <Sky />
 
           <Island 
             position={islandPosition}
