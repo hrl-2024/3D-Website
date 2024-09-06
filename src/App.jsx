82941a1dbd7947a4fca52a85f5fa1d7bb41d751f
Ruihang <hrl@bu.edu>
2024-09-06 14:20:30 -0400

    Fix url problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import { Home, About, Projects, Contact } from './pages'
 
+const base = import.meta.env.VITE_BASE_URL;
+
 const App = () => {
   return (
     <main className="bg-slate-300/20">
         <Router>
-            <Navbar />
+            <Navbar baseurl={base}/>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<Contact />} />
+                <Route path={`${base}`} element={<Home />} />
+                <Route path={`${base}about`} element={<About />} />
+                <Route path={`${base}projects`} element={<Projects />} />
+                <Route path={`${base}contact`} element={<Contact />} />
             </Routes>
         </Router>
     </main>
