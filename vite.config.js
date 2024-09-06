e7677f8ddfd79890811ceb6d1dacd9e92fe2062a
Ruihang <hrl@bu.edu>
2024-09-06 12:50:05 -0400

    file still not found

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/3D-Website/src',
+  base: '/3D-Website/',
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
