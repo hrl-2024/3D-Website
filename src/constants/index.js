863699038d7e6167d8c35a3ec5ae264c169c70bc
Ruihang <hrl@bu.edu>
2024-11-20 16:55:59 -0500

    New project and formatting

--- src/constants/index.js
+++ src/constants/index.js
@@ -111,29 +111,29 @@ export const skills = [
 
 export const experiences = [
     {
-      title: "Software Engineer Intern",
-      company_name: "iQiyi.com",
-      icon: icons.iQiyi,
-      iconBg: "#E6DEDD",
-      date: "Jun 2023 - Aug 2023",
-      points: [
-        "Architected with Spring Boot framework to enhance the performance of iQiyi's 110.7 million users' Big Data Platform, reducing response time from 3 mins to 20 secs with multi-threaded system design.",
-        "Crafted scalable APIs to bridge backend technologies with front-end UIs for data scientists.",
-      ],
+        title: "Software Engineer Intern",
+        company_name: "iQiyi.com",
+        icon: icons.iQiyi,
+        iconBg: "#E6DEDD",
+        date: "Jun 2023 - Aug 2023",
+        points: [
+            "Architected with Spring Boot framework to enhance the performance of iQiyi's 110.7 million users' Big Data Platform, reducing response time from 3 mins to 20 secs with multi-threaded system design.",
+            "Crafted scalable APIs to bridge backend technologies with front-end UIs for data scientists.",
+        ],
     },
     {
-      title: "Undergraduate Teaching/Course Assistant",
-      company_name: "Boston University Department of Computer Science",
-      icon: "https://upload.wikimedia.org/wikipedia/commons/3/31/Boston_University_wordmark.svg",
-      iconBg: "#CC0000",
-      date: "Sep 2022 - Dec 2022; Jan 2024 - Current",
-      points: [
-        "Tutored 180+ students in functional programming, OCaml and Android application development in office hours.",
-        "Participated in assignment making to enhance student learning.",
-        "Created and graded assignments of 180+ students within a 4-day turnaround.",
-      ],
+        title: "Undergraduate Teaching/Course Assistant",
+        company_name: "Boston University Department of Computer Science",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/3/31/Boston_University_wordmark.svg",
+        iconBg: "#CC0000",
+        date: "Sep 2022 - Dec 2022; Jan 2024 - Current",
+        points: [
+            "Tutored 180+ students in functional programming, OCaml and Android application development in office hours.",
+            "Participated in assignment making to enhance student learning.",
+            "Created and graded assignments of 180+ students within a 4-day turnaround.",
+        ],
     },
-  ]
+]
 
 export const socialLinks = [
     {
@@ -155,36 +155,42 @@ export const socialLinks = [
 
 export const projects = [
     {
-      iconUrl: icons.ai,
-      theme: 'btn-back-blue',
-      name: 'ChessMS',
-      description: 'Developed ChessMS, a chess AI agent on SMS Text, which won at BostonHack 2022. Designed and deployed a serverless database using CockroachDB and connected a local Flask server with Twilio clients using ngrok and WebHook.',
-      link: 'https://devpost.com/software/chessms-8ta6j9',
+        iconUrl: icons.ai,
+        theme: 'btn-back-blue',
+        name: 'F1Tenth Autonomous Racing Car',
+        description: 'Developed a ROS-based autonomous racing car using a LIDAR sensor and a camera. Built a Unity-based simulation for the autonomous racing car to streamline reinforcement learning workflows. Researched Gaussian Splatting and reinforcement learning algorithms to optimize race car\'s driving performance. ',
     },
     {
-      iconUrl: icons.doodlejump,
-      theme: 'btn-back-green',
-      name: 'Doodle Jump AI',
-      description: 'Created an AI agent to play Doodle Jump using the NEAT algorithm, a genetic AI algorithm. Explored various input features and reward systems, adjusting NEAT algorithm parameters for optimal results.',
-      link: 'https://github.com/alexjmiller5/DoodleJump-NEAT-AI',
+        iconUrl: icons.ai,
+        theme: 'btn-back-blue',
+        name: 'ChessMS',
+        description: 'Developed ChessMS, a chess AI agent on SMS Text, which won at BostonHack 2022. Designed and deployed a serverless database using CockroachDB and connected a local Flask server with Twilio clients using ngrok and WebHook.',
+        link: 'https://devpost.com/software/chessms-8ta6j9',
     },
     {
-      iconUrl: icons.ai,
-      theme: 'btn-back-pink',
-      name: 'AI Ball Balancing Robot',
-      description: 'Leveraged reinforcement learning techniques to train an AI agent for real-time ball balancing on a robot. Engineered bespoke mechanisms with signal processing capabilities, using UART communication to translate AI predictions into precise robotic arm movements.'
+        iconUrl: icons.doodlejump,
+        theme: 'btn-back-green',
+        name: 'Doodle Jump AI',
+        description: 'Created an AI agent to play Doodle Jump using the NEAT algorithm, a genetic AI algorithm. Explored various input features and reward systems, adjusting NEAT algorithm parameters for optimal results.',
+        link: 'https://github.com/alexjmiller5/DoodleJump-NEAT-AI',
     },
     {
-      iconUrl: icons.moviedate,
-      theme: 'btn-back-yellow',
-      name: 'MovieDate',
-      description: 'Architected backend server and wrote HTTP RESTFUL API for a social media application. Enhanced relational database performance with indexes, foreign keys, and database design, balancing storage and speed. Deployed CockroachDB for high availability and throughput.',
-      link: 'https://github.com/hrl-2024/MovieDate',
+        iconUrl: icons.ai,
+        theme: 'btn-back-pink',
+        name: 'AI Ball Balancing Robot',
+        description: 'Leveraged reinforcement learning techniques to train an AI agent for real-time ball balancing on a robot. Engineered bespoke mechanisms with signal processing capabilities, using UART communication to translate AI predictions into precise robotic arm movements.'
     },
     {
-      iconUrl: cloudcomputing,
-      theme: 'btn-back-purple',
-      name: 'Distributed Data Processing Systems',
-      description: 'Developed a custom MapReduce system in Go, employing parallel computation to optimize distributed data processing. Engineered a RAFT-based distributed system in Go for robust log consensus in a distributed setting.'
+        iconUrl: icons.moviedate,
+        theme: 'btn-back-yellow',
+        name: 'MovieDate',
+        description: 'Architected backend server and wrote HTTP RESTFUL API for a social media application. Enhanced relational database performance with indexes, foreign keys, and database design, balancing storage and speed. Deployed CockroachDB for high availability and throughput.',
+        link: 'https://github.com/hrl-2024/MovieDate',
     },
-  ]
+    {
+        iconUrl: cloudcomputing,
+        theme: 'btn-back-purple',
+        name: 'Distributed Data Processing Systems',
+        description: 'Developed a custom MapReduce system in Go, employing parallel computation to optimize distributed data processing. Engineered a RAFT-based distributed system in Go for robust log consensus in a distributed setting.'
+    },
+]
