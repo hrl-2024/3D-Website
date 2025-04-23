602ab13a6f2e240bf3fe1773f8aaabaf606989d8
hrl <hrl@bu.edu>
2025-04-23 18:47:31 -0400

    Fixed link issue

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -26,21 +26,21 @@ const renderContend = {
     2: (
         <InfoBox
             text="Looking for full time SWE job"
-            link = {`${base}about`}
+            link = {`/about`}
             btnText="Learn More"
         />
     ),
     3: (
         <InfoBox
             text="Worked on multiple projects"
-            link = {`${base}projects`}
+            link = {`/projects`}
             btnText="Visit my portfolio"
         />
     ),
     4: (
         <InfoBox
             text="Need a project done or looking for a dev? I'm just a few keystrokes away"
-            link = {`${base}contact`}
+            link = {`/contact`}
             btnText="Let's Talk"
         />
     )
